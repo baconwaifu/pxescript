b6e3362d29c8012fb0912fe1a0cc0e7755035e31
slango20 <slango22@gmail.com>
2016-10-22 13:55:25 -0400

    Add special entry support to config generator, change special entry format

--- pxeupdate.py
+++ pxeupdate.py
@@ -3,9 +3,9 @@
 #Description: PXE Netboot Repository tree updater
 #Takes options as arrays set in the file
 #TODO: set up arguments
-#TODO: Config files
+#TODO: Config files for the script
 #TODO: Add support for non-debian based repository trees (can arch netboot?)
-#TODO: find a better way to download files
+#TODO: find a better way to download files that will skip if no changes (similar to rsync -u)
 #TODO low: add per-file progress bar without bloated wget UI
 #TODO: Make Modular (add classes for common distros to eliminate the looped distro-specific handling)
 #TODO low: threading
@@ -26,8 +26,8 @@ dists = ["debian"]
 rels = ["jessie", "sid"]
 #Files to get from the repository, must be populated
 targets = ["linux", "initrd.gz"]
-#Special targets in a wget string followed by a path, need to make a class
-special = [["http://archive.ubuntu.com/ubuntu/dists/trusty/main/uefi/grub2-amd64/current/grubnetx64.efi.signed -O special/grub/amd64/grubnetx64.efi.signed", "special/grub/amd64/grubnetx64.efi.signed"]]
+#Special targets, [0] is the name for the config generator, [1] is the URL the file can be found at, and [2] is the path for both the config generator and wget. [3] is fancy name
+special = [["grub_efi_amd64", "http://archive.ubuntu.com/ubuntu/dists/trusty/main/uefi/grub2-amd64/current/grubnetx64.efi.signed", "special/grub/amd64/grubnetx64.efi.signed", "amd64 GRUB EFI Netboot"]]
 
 for dist in dists:
     for rel in rels:
@@ -46,7 +46,7 @@ for dist in dists:
                     installer=arch+"-installer"
                 os.system("wget "+archive+dist+"/dists/" + rel + "/main/"+installer+"/current/images/netboot/"+dist+"-installer/"+arch+"/"+target+" -O "+outfile+" >/dev/null 2>&1")
 for i in special:
-    os.system("wget "+i[0]+suppress) #Account for array
+    os.system("wget "+i[1]+" -O "+i[2]+suppress) #Account for array
 print("Generating Configuration File...")
 conffile = open("default", "w")
 conffile.write("default menu.c32\nmenu title PXE Boot System\ntimeout 32\n\n")
@@ -56,6 +56,9 @@ for dist in dists:
             print("Adding "+dist+"_"+rel+"_"+arch+"...")
             conffile.write("lable "+dist+"_"+rel+"_"+arch+"\nmenu label "+dist+" "+rel+" "+arch+"\nkernel http://pxeserver."+domain+"/"+dist+"/"+rel+"/"+arch+"/linux\n")
             conffile.write("append initrd=http://pxeserver."+domain+"/"+dist+"/"+rel+"/"+arch+"/initrd.img\n\n")
+for spec in special:
+    conffile.write("lable "+spec[0]+"\nmenu lable "+spec[3]+"\nkernel "+spec[2]) #TODO: Make this more flexible
+conffile.close() #technically uneeded unless this is being called directly into another python script
 # Example generated entry:
 # lable debian_jessie_amd64
 # menu lable debian jessie amd64
